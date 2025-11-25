367e6f3e89ebabf50ef7f9673f107f4678c6e986
Ryan <hcg1228@foxmail.com>
2025-11-25 12:30:44 +0800

    use svg icon

---
+++ AshellWPF/StringIconConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AshellWPF
+{
+    public class StringIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return Application.Current.FindResource((string)value);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
